85dd5fdd1a001e4ea90f5cba4145e6ce3d649ce7
robertwgh <robertwgh@gmail.com>
2018-07-03 15:31:02 -0700

    move CMakeLists.txt to platforms/desktop

---
+++ platforms/desktop/build/bin/examples/image_match/CMakeFiles/image_match.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_match.dir/image_match.cpp.o"
+  "../../image_match.pdb"
+  "../../image_match"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_match.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
