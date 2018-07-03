9b53c9e689258182b30b06c0afb6e43a17a32a73
robertwgh <robertwgh@gmail.com>
2018-07-03 01:29:10 -0700

    re-format

--- include/ezsift.h
+++ include/ezsift.h
@@ -1,24 +1,24 @@
-/*	Copyright (c) 2013, Robert Wang, email: robertwgh (at) gmail.com
-	All rights reserved. https://github.com/robertwgh/ezSIFT
+/* Copyright (c) 2013, Robert Wang, email: robertwgh (at) gmail.com
+   All rights reserved. https://github.com/robertwgh/ezSIFT
 
-	Some algorithms used in this code referred to:
-	1. OpenCV: http://opencv.org/
-	2. VLFeat: http://www.vlfeat.org/
+   Some algorithms used in this code referred to:
+   1. OpenCV: http://opencv.org/
+   2. VLFeat: http://www.vlfeat.org/
 
-	The SIFT algorithm was developed by David Lowe. More information can be found from:
-	David G. Lowe, "Distinctive image features from scale-invariant keypoints," 
-	International Journal of Computer Vision, 60, 2 (2004), pp. 91-110.
+   The SIFT algorithm was developed by David Lowe. More information can be found from:
+   David G. Lowe, "Distinctive image features from scale-invariant keypoints," 
+   International Journal of Computer Vision, 60, 2 (2004), pp. 91-110.
 
-	Pay attention that the SIFT algorithm is patented. It is your responsibility to use the code
-	in a legal way. Patent information:
-	Method and apparatus for identifying scale invariant features in an image 
-	and use of same for locating an object in an image	David G. Lowe, US Patent 6,711,293 
-	(March 23, 2004). Provisional application filed March 8, 1999. Asignee: The University of 
-	British Columbia.
+   Pay attention that the SIFT algorithm is patented. It is your responsibility to use the code
+   in a legal way. Patent information:
+   Method and apparatus for identifying scale invariant features in an image 
+   and use of same for locating an object in an image David G. Lowe, US Patent 6,711,293 
+   (March 23, 2004). Provisional application filed March 8, 1999. Asignee: The University of 
+   British Columbia.
 
-	Revision history:
-		September, 15, 2013: initial version.
-		July 8th, 2014, re-organized source code. 
+   Revision history:
+      September, 15, 2013: initial version.
+      July 8th, 2014, re-organized source code. 
 */
 
 #ifndef EZSIFT_H
@@ -118,66 +118,66 @@ static const int SIFT_FIXPT_SCALE = 1;
 #define DEGREE_OF_DESCRIPTORS (128)
 typedef struct _SiftKeypoint
 {
-	int octave; // octave number
-	int layer;  // layer number
-	float rlayer; // real number of layer number
+   int octave; // octave number
+   int layer;  // layer number
+   float rlayer; // real number of layer number
 
-	float r; // normalized row coordinate
-	float c; // normalized col coordinate
-	float scale; // normalized scale
+   float r; // normalized row coordinate
+   float c; // normalized col coordinate
+   float scale; // normalized scale
 
-	float ri;	//row coordinate in that layer. 
-	float ci;	//column coordinate in that layer.
-	float layer_scale; // the scale of that layer
+   float ri;   //row coordinate in that layer. 
+   float ci;   //column coordinate in that layer.
+   float layer_scale; // the scale of that layer
 
-	float ori; // orientation in degrees.
-	float mag; // magnitude
+   float ori; // orientation in degrees.
+   float mag; // magnitude
 
-	float descriptors[DEGREE_OF_DESCRIPTORS];
+   float descriptors[DEGREE_OF_DESCRIPTORS];
 } SiftKeypoint;
 
 // Match pair structure. Use for interest point matching.
 typedef struct _MatchPair
 {
-	int r1;
-	int c1;
-	int r2;
-	int c2;
+   int r1;
+   int c1;
+   int r2;
+   int c2;
 } MatchPair;
 
 
 // Combine two images for interest points matching.
 // Images are combined horizontally.
 int combine_image(
-	Image<unsigned char> & out_image,
-	const Image<unsigned char> & image1,
-	const Image<unsigned char> & image2);
+   Image<unsigned char> & out_image,
+   const Image<unsigned char> & image1,
+   const Image<unsigned char> & image2);
 
 // Draw circles to incidate the keypoints.
 // Bars in the circle incidate the orientation of the keypoints.
 void  draw_keypoints_to_ppm_file(
-	const char* out_filename,
-	const Image<unsigned char> & image, 
-	std::list<SiftKeypoint> kpt_list);
+   const char* out_filename,
+   const Image<unsigned char> & image, 
+   std::list<SiftKeypoint> kpt_list);
 
 // Draw lines between matched keypoints.
 // The result image is stored in a ppm file.
 int draw_match_lines_to_ppm_file(
-	const char * filename,
-	Image<unsigned char> & image1,
-	Image<unsigned char> & image2,
-	std::list<MatchPair> & match_list);
+   const char * filename,
+   Image<unsigned char> & image1,
+   Image<unsigned char> & image2,
+   std::list<MatchPair> & match_list);
 
 // Draw matched lines on a color RGB image.
 int draw_line_to_rgb_image(
-	const unsigned char* & data,
-	int w, int h,
-	MatchPair & mp);
+   const unsigned char* & data,
+   int w, int h,
+   MatchPair & mp);
 
 // Draw matched lines on an Image object.
 int draw_line_to_image(
-	Image<unsigned char> & image, 
-	MatchPair & mp);
+   Image<unsigned char> & image, 
+   MatchPair & mp);
 
 
 /****************************************
@@ -185,10 +185,10 @@ int draw_line_to_image(
  ***************************************/
 // Initialize SIFT parameters.
 void init_sift_parameters(
-	bool doubleFirstOctave = true,
-	float contrast_threshold = 8.0f,
-	float edge_threshold = 10.0f,
-	float match_NDDR_threshold = 0.6f);
+   bool doubleFirstOctave = true,
+   float contrast_threshold = 8.0f,
+   float edge_threshold = 10.0f,
+   float match_NDDR_threshold = 0.6f);
 
 // Enable doubling of original image.
 void double_original_image(bool doubleFirstOctave);
@@ -197,67 +197,67 @@ void double_original_image(bool doubleFirstOctave);
 // 1. Use row buf to handle border pixel.
 // 2. hori processing and transpose
 int gaussian_blur(
-	const Image<float> & in_image, 
-	Image<float> & out_image, 
-	std::vector<float> coef1d);
+   const Image<float> & in_image, 
+   Image<float> & out_image, 
+   std::vector<float> coef1d);
 
 // Row filter and then transpose
 int row_filter_transpose(
-	float * src, 
-	float * dst, 
-	int w, int h,
-	float * coef1d, int gR);
+   float * src, 
+   float * dst, 
+   int w, int h,
+   float * coef1d, int gR);
 
 // Build image octaves during the initialization.
 int build_octaves(const Image<unsigned char> &image, 
-	std::vector<Image<unsigned char> > & octaves, 
-	int firstOctave, 
-	int nOctaves);
+   std::vector<Image<unsigned char> > & octaves, 
+   int firstOctave, 
+   int nOctaves);
 
 // Compute Gaussian filter coefficients for Gaussian Blur.
 std::vector< std::vector<float> > 
-	compute_gaussian_coefs(int nOctaves, int nGpyrLayers);
+   compute_gaussian_coefs(int nOctaves, int nGpyrLayers);
 
 // Build Gaussian pyramid.
 int build_gaussian_pyramid(
-	std::vector<Image<unsigned char> > & octaves,
-	std::vector<Image<float> > & gpyr, 
-	int nOctaves, 
-	int nGpyrLayers);
+   std::vector<Image<unsigned char> > & octaves,
+   std::vector<Image<float> > & gpyr, 
+   int nOctaves, 
+   int nGpyrLayers);
 
 // Build DoG pyramid. 
 int build_dog_pyr(
-	std::vector<Image<float> > & gpyr, 
-	std::vector<Image<float> > & dogPyr, 
-	int nOctaves, 
-	int nDogLayers);
+   std::vector<Image<float> > & gpyr, 
+   std::vector<Image<float> > & dogPyr, 
+   int nOctaves, 
+   int nDogLayers);
 
 // Build gradient and rotation pyramid.
 int build_grd_rot_pyr(
-	std::vector<Image<float> > & gpyr, 
-	std::vector<Image<float> > & grdPyr, 
-	std::vector<Image<float> > & rotPyr, 
-	int nOctaves, 
-	int nLayers);
+   std::vector<Image<float> > & gpyr, 
+   std::vector<Image<float> > & grdPyr, 
+   std::vector<Image<float> > & rotPyr, 
+   int nOctaves, 
+   int nLayers);
 
 // Refine local extrema.
 bool refine_local_extrema(
-	std::vector<Image<float> > & dogPyr, 
-	int nOctaves, 
-	int nDogLayers, 
-	SiftKeypoint & kpt);
+   std::vector<Image<float> > & dogPyr, 
+   int nOctaves, 
+   int nDogLayers, 
+   SiftKeypoint & kpt);
 
 // Export keypoint list to a file.
 int export_kpt_list_to_file(
-	const char * filename, 
-	std::list<SiftKeypoint> & kpt_list,
-	bool bIncludeDescpritor);
+   const char * filename, 
+   std::list<SiftKeypoint> & kpt_list,
+   bool bIncludeDescpritor);
 
 // Compute orientation histogram.
 float compute_orientation_hist(
-	const Image<float> & image, 
-	SiftKeypoint & kpt,
-	float * & hist);
+   const Image<float> & image, 
+   SiftKeypoint & kpt,
+   float * & hist);
 
 
 /****************************************
@@ -265,20 +265,20 @@ float compute_orientation_hist(
  ***************************************/
 // Detect keypoints.
 int detect_keypoints(
-	std::vector< Image<float> > & dogPyr, 
-	std::vector< Image<float> > & grdPyr, 
-	std::vector< Image<float> > & rotPyr, 
-	int nOctaves, 
-	int nDogLayers, 
-	std::list<SiftKeypoint> & kpt_list);
+   std::vector< Image<float> > & dogPyr, 
+   std::vector< Image<float> > & grdPyr, 
+   std::vector< Image<float> > & rotPyr, 
+   int nOctaves, 
+   int nDogLayers, 
+   std::list<SiftKeypoint> & kpt_list);
 
 // Extract descriptor.
 int extract_descriptor(
-	std::vector< Image<float> > & grdPyr, 
-	std::vector< Image<float> > & rotPyr, 
-	int nOctaves,
-	int nGpyrLayers,
-	std::list<SiftKeypoint> & kpt_list);
+   std::vector< Image<float> > & grdPyr, 
+   std::vector< Image<float> > & rotPyr, 
+   int nOctaves,
+   int nGpyrLayers,
+   std::list<SiftKeypoint> & kpt_list);
 
 
 /****************************************
@@ -286,15 +286,15 @@ int extract_descriptor(
  ***************************************/
 // Detect keypoints and extract descriptor.
 int sift_cpu(
-	const Image<unsigned char> &image, 
-	std::list<SiftKeypoint> & kpt_list, 
-	bool bExtractDescriptors);
+   const Image<unsigned char> &image, 
+   std::list<SiftKeypoint> & kpt_list, 
+   bool bExtractDescriptors);
 
 // Match keypoints from two keypoint lists.
 int match_keypoints(
-	std::list<SiftKeypoint> & kpt_list1,
-	std::list<SiftKeypoint> & kpt_list2,
-	std::list<MatchPair> & match_list);
+   std::list<SiftKeypoint> & kpt_list1,
+   std::list<SiftKeypoint> & kpt_list2,
+   std::list<MatchPair> & match_list);
 
 } // end namespace ezsift
 
