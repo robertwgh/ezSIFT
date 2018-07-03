1b4e9febfb0563fd38d7a44225db81fc3f98576e
robertwgh <robertwgh@gmail.com>
2018-07-03 11:16:30 -0700

    fix android script

--- platforms/android/install_and_run.sh
+++ platforms/android/install_and_run.sh
@@ -6,8 +6,16 @@ adb push ../../data/img1.pgm /data/local/tmp/ezsift/bin/
 adb push ../../data/img2.pgm /data/local/tmp/ezsift/bin/
 adb shell 'chmod 777 -R /data/local/tmp/ezsift/bin'
 
-adb shell "adb shell "cd /data/local/tmp/ezsift/bin && \
-           ./feature_extract img1.pgm""
+echo "\n******************************"
+echo "* Running feature extraction *"
+echo "******************************"
+adb shell "cd /data/local/tmp/ezsift/bin && \
+           export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH && \
+           ./feature_extract img1.pgm"
 
-adb shell "adb shell "cd /data/local/tmp/ezsift/bin && \
-	       ./image_match img1.pgm img2.pgm""
+echo "\n***********************"
+echo "* Running image match *"
+echo "***********************"
+adb shell "cd /data/local/tmp/ezsift/bin && \
+           export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH && \
+           ./image_match img1.pgm img2.pgm"
