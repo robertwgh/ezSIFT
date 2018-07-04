b3be3312e9f517ea1ab798e3e5f304b85cc3784d
robertwgh <robertwgh@gmail.com>
2018-07-03 18:16:53 -0700

    update scripts

--- scripts/ci_build_desktop.sh
+++ scripts/ci_build_desktop.sh
@@ -1,6 +1,5 @@
 echo "build for desktop"
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${dir}/../platforms/desktop
+cd $(pwd)/../platforms/desktop
 mkdir -p build
 cd build
 cmake ..
--- scripts/ci_test_desktop.sh
+++ scripts/ci_test_desktop.sh
@@ -1,5 +1,4 @@
 echo "run examples for desktop"
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ${dir}/../platforms/desktop/build/bin
+cd $(pwd)/../platforms/desktop/build/bin
 ./image_match img1.pgm img2.pgm
 ./feature_extract img1.pgm
